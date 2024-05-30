1923705ca8d1b1e9ee576bc280670c03868d972a
Gan Yuanrong <372129081@qq.com>
2024-05-30 11:15:31 +0800

    修改加密类

--- modifyXcodeProject/utils/PrpCrypt.py
+++ modifyXcodeProject/utils/PrpCrypt.py
@@ -30,12 +30,38 @@ class PrpCrypt(object):
         # 填充函数
         # self.padding = lambda data: data + (self.block_size - len(data) % self.block_size) * chr(self.block_size - len(data) % self.block_size)
         # 此处为一坑,需要现将data转换为byte再来做填充，否则中文特殊字符等会报错
-        self.padding = lambda data: data + (self.block_size - len(data.encode('utf-8')) % self.block_size) * chr(
-            self.block_size - len(data.encode('utf-8')) % self.block_size)
+        # self.padding_for_str = lambda data: data + (self.block_size - len(data.encode('utf-8')) % self.block_size) * chr(
+        #     self.block_size - len(data.encode('utf-8')) % self.block_size)
+
+        self.padding_for_data = lambda data: data + (self.block_size - len(data) % self.block_size) * chr(
+            self.block_size - len(data) % self.block_size)
+
+        # bs = AES.block_size
+        # pad = lambda s: s + (bs - len(s) % bs) * chr(bs - len(s) % bs)
+
         # 截断函数
         self.unpadding = lambda data: data[:-ord(data[-1])]
 
-    def aes_encrypt(self, plaintext):
+
+    def encrypt_for_data(self, file_data):
+        # cipher = AES.new(self.key, self.mode, self.iv)
+        # base64_text = base64.b64encode(file_data)
+        # ciphertext = self.aes_encrypt(base64_text)
+
+        try:
+            # 填充16位
+            padding_text = self.padding_for_data(file_data)
+            # 初始化加密器
+            cryptor = AES.new(self.key, self.mode, self.iv)
+            # 进行AES加密
+            encrypt_aes = cryptor.encrypt(padding_text)
+            # 进行BASE64转码
+            # encrypt_text = (base64.b64encode(encrypt_aes)).decode()
+            return encrypt_aes
+        except Exception as e:
+            logging.exception(e)
+
+    def aes_encrypt_base64(self, plaintext):
         """
         加密
         :param plaintext: 明文
@@ -43,18 +69,19 @@ class PrpCrypt(object):
         """
         try:
             # 填充16位
-            padding_text = self.padding(plaintext).encode("utf-8")
-            # 初始化加密器
-            cryptor = AES.new(self.key, self.mode, self.iv)
-            # 进行AES加密
-            encrypt_aes = cryptor.encrypt(padding_text)
+            # padding_text = self.padding_for_str(plaintext).encode("utf-8")
+            # # 初始化加密器
+            # cryptor = AES.new(self.key, self.mode, self.iv)
+            # # 进行AES加密
+            # encrypt_aes = cryptor.encrypt(padding_text)
+            encrypt_aes = self.encrypt_for_data(plaintext)
             # 进行BASE64转码
-            encrypt_text = (base64.b64encode(encrypt_aes)).decode()
+            encrypt_text = (base64.b64encode(encrypt_aes)) #.decode()
             return encrypt_text
         except Exception as e:
             logging.exception(e)
 
-    def aes_decrypt(self, ciphertext):
+    def aes_decrypt_base64(self, ciphertext):
         """
         解密
         :param ciphertext: 密文
@@ -70,8 +97,8 @@ class PrpCrypt(object):
             # 进行ASE解密
             decrypt_text = cryptor.decrypt(plain_base64)
             # 截取
-            plain_text = self.unpadding(decrypt_text.decode("utf-8"))
-            return plain_text
+            decrypt_text = self.unpadding(decrypt_text)
+            return decrypt_text
         except UnicodeDecodeError as e:
             logging.error('解密失败,请检查密钥是否正确!')
             logging.exception(e)
@@ -121,8 +148,8 @@ if __name__ == '__main__':
     # data = input("请输入待加密数据：")#
     data = 'userId'
 
-    aes_decrypt_a = pc.aes_encrypt(data)
+    aes_decrypt_a = pc.aes_encrypt_base64(data)
     print("aes_encrypt2:", aes_decrypt_a)
     aes_decode_data = aes_decrypt_a
-    aes_decrypt_a = pc.aes_decrypt(aes_decode_data)
+    aes_decrypt_a = pc.aes_decrypt_base64(aes_decode_data)
     print("aes_decrypt2:", aes_decrypt_a)
